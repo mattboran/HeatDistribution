d059dacce1c623478808bcd27c02134d21732eaa
mattboran <mattboran@gmail.com>
2018-10-01 00:01:05 -0400

    Fixed tests dir

--- tests/exp1.sh
+++ tests/exp1.sh
@@ -2,11 +2,11 @@ echo "Experiment 1"
 echo "CPU version"
 for i in 1000 2000 4000 8000 16000 32000
 do
-  ./heatdist 1000 $i 0
+  ../heatdist 1000 $i 0
 done
 
 echo "GPU version"
 for i in 1000 2000 4000	8000 16000 32000
 do
-  ./heatdist 1000 $i 1
+  ../heatdist 1000 $i 1
 done
